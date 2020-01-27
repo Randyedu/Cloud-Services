8bf595fbcc1174a5f17de1412d587867e702fe0c
Randy Guo <44625808@qq.com>
2020-01-27 21:02:08 +0800

    Update pip-upgrade-package.py

--- pip-upgrade-package.py
+++ pip-upgrade-package.py
@@ -9,3 +9,17 @@ cc = json.loads(cc)
 for dist in cc:
     print(dist['name'])
     call("pip3 install --upgrade " + dist['name'], shell=True)
+
+
+
+from subprocess import call
+import os
+import json
+
+cc = os.popen('pip list --outdate --format=json')
+cc = cc.read()
+print(cc)
+cc = json.loads(cc)
+for dist in cc:
+    print(dist['name'])
+    call("pip3 uninstall -y " + dist['name'], shell=True)

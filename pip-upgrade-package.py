06493808840f4f3c62dc9a87e8a8ee6e73da2e35
Randy Guo <1155074290@link.cuhk.edu.hk>
2020-01-27 12:43:11 +0800

    Create pip-upgrade-package.py

---
+++ pip-upgrade-package.py
@@ -0,0 +1,11 @@
+from subprocess import call
+import os
+import json
+
+cc = os.popen('pip list --outdate --format=json')
+cc = cc.read()
+print(cc)
+cc = json.loads(cc)
+for dist in cc:
+    print(dist['name'])
+    call("pip3 install --upgrade " + dist['name'], shell=True)
